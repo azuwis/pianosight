ebda1ef7a85bdebee97621d34602991b482a5626
Zhong Jianxin <azuwis@gmail.com>
2020-07-08 17:29:15 +0800

    Reload window when service worker updates

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
+import { Workbox } from 'workbox-window';
 import App from './App.svelte';
 import './index.css';
 
 if (process.env.NODE_ENV === 'production') {
   if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js');
+    const wb = new Workbox('/service-worker.js');
+
+    wb.addEventListener('installed', event => {
+      if (event.isUpdate) {
+        window.location.reload();
+      }
     });
+
+    wb.register();
   }
 }
 
