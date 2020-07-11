6faf0abbdad08f44a2a19113e2421b0da451d865
Zhong Jianxin <azuwis@gmail.com>
2020-07-11 23:55:43 +0800

    Use -ml-1 to workaound a alignment problem

--- src/App.svelte
+++ src/App.svelte
@@ -12,16 +12,20 @@ let file
 </script>
 
 <div class="flex flex-col h-screen">
-  <div class="flex flex-wrap items-center justify-center">
-    <div class="flex flex-wrap items-center justify-center space-x-1">
-      <div class="w-48 mt-1 ml-1">
-        <File bind:this={file}/>
-      </div>
-      <div class="w-48 mt-1 mr-1">
-        <Midi/>
+  <div class="flex flex-wrap items-center justify-center -ml-1">
+    <div class="ml-1">
+      <div class="flex flex-wrap items-center justify-center -ml-1">
+        <div class="w-48 mt-1 ml-1">
+          <File bind:this={file}/>
+        </div>
+        <div class="w-48 mt-1 ml-1">
+          <Midi/>
+        </div>
       </div>
     </div>
-    <Nav {sheet}/>
+    <div class="ml-1">
+      <Nav {sheet}/>
+    </div>
   </div>
   <div class="flex-1 overflow-y-auto lg:px-4">
     <Sheet {file} bind:this={sheet}/>
