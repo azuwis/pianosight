59ef5b24a88e6105dc8294715ecb5290834144c3
Zhong Jianxin <azuwis@gmail.com>
2020-07-17 19:56:19 +0800

    SheetGenerator: Set complexity

--- src/SheetGenerator.svelte
+++ src/SheetGenerator.svelte
@@ -6,9 +6,8 @@ import {
   RhythmSymbolEnum,
 } from 'opensheetmusicdisplay'
 import {
-  DurationSettings,
+  ComplexityMap,
   ExampleSourceGenerator,
-  PitchSettings,
   ScaleKey,
   XMLSourceExporter,
 } from './osme.min.js'
@@ -19,8 +18,8 @@ import {
 
 let inputScale
 let inputTime = 4
+let inputComplexity = 2
 let inputMeasure = 16
-let inputPitch
 
 const scaleOptions = [
   ['major_0-0', 'C'],
@@ -38,13 +37,14 @@ const scaleOptions = [
   ['major_4-b', 'Gb'],
 ]
 const timeOptions = [2, 3, 4]
+const complexityOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const measureOptions = [8, 16, 32, 48, 64, 96]
 
 export function generate() {
   const time_signature = new RhythmInstruction(new Fraction(inputTime, 4, 0, false), RhythmSymbolEnum.NONE)
   const scale_key = ScaleKey.fromStringCode(inputScale)
-  const pitch_settings = PitchSettings.ALL().get('MINIMAL')
-  const duration_settings = DurationSettings.ALL().get('MINIMAL')
+  const pitch_settings = ComplexityMap.getPitchSettings(inputComplexity)
+  const duration_settings = ComplexityMap.getDurationSettings(inputComplexity)
   const rules = new EngravingRules()
   const options = {
     time_signature,
@@ -61,6 +61,7 @@ export function generate() {
 }
 </script>
 
+<!-- svelte-ignore a11y-no-onchange -->
 <div class:hidden={!$showSheetGenerator} class="ml-1">
   <div class="flex flex-wrap items-center justify-center -ml-1">
     <div class="w-auto mt-1 ml-1">
@@ -78,6 +79,16 @@ export function generate() {
         {#each timeOptions as time}
         <option value={time}>
           {time}/4
+        </option>
+        {/each}
+      </select>
+    </div>
+
+    <div class="w-auto mt-1 ml-1">
+      <select bind:value={inputComplexity} on:change={generate} class="form-select form-complexity" title="Complexity">
+        {#each complexityOptions as complexity}
+        <option value={complexity}>
+          {complexity}
         </option>
         {/each}
       </select>
@@ -110,6 +121,11 @@ export function generate() {
 .form-time {
   background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' version='1.0' viewBox='0 0 20 58' fill='%23718096'%3E%3Cpath d='M10.95 17.17h.604a6.992 6.992 0 013.13.742 7.125 7.125 0 012.433 1.994 2.843 2.843 0 01-.324-.023 2.856 2.856 0 00-.325-.024c-1.052.039-1.869.414-2.451 1.125a3.81 3.81 0 00-.887 2.445c.07 1.261.498 2.16 1.287 2.695.788.536 1.518.797 2.19.783a3 3 0 002.18-.991c.593-.635.902-1.417.927-2.347 0-.911-.304-1.949-.91-3.114-.607-1.165-1.515-2.185-2.724-3.062-1.209-.876-2.718-1.337-4.526-1.382h-.603l-1.716.14c-3.327.604-5.713 2.014-7.157 4.23-1.444 2.216-2.15 4.808-2.116 7.778-.03 2.969.68 5.561 2.133 7.777 1.453 2.216 3.833 3.626 7.14 4.231l1.716.14c.115.001.221.009.319.022.097.014.192.022.284.024 2.06-.083 3.714-.711 4.96-1.886 1.247-1.175 2.149-2.4 2.705-3.678.557-1.278.83-2.112.82-2.504a.526.526 0 00-.198-.4.647.647 0 00-.405-.156.54.54 0 00-.33.11.66.66 0 00-.227.353c-.459 1.935-1.326 3.571-2.602 4.91-1.276 1.337-2.85 2.034-4.723 2.091a2.448 2.448 0 01-.284-.023c-.098-.013-.204-.021-1.177-.185-2.156-.69-2.979-1.674-3.326-3.112-.348-1.439-.499-3.222-.453-5.35v-4.729c-.046-2.128.105-3.911.453-5.35.347-1.438 1.17-2.42 2.468-2.95'/%3E%3C/svg%3E");
   background-size: 1em 2em;
+}
+
+.form-complexity {
+  background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 20 20' fill='%23718096' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M3.333 8L10 12l10-6-10-6L0 6h10v2H3.333zM0 8v8l2-2.222V9.2L0 8zm10 12l-5-3-2-1.2v-6l7 4.2 7-4.2v6L10 20z'/%3E%3C/svg%3E");
+  background-size: 1em 1em;
 }
 
 .form-measure {
