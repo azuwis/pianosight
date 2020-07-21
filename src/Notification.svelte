4c1be47d2b2211bfbe228caf685cac68a6c1c50f
Zhong Jianxin <azuwis@gmail.com>
2020-07-21 20:02:27 +0800

    Notification: Add transition

--- src/Notification.svelte
+++ src/Notification.svelte
@@ -1,9 +1,10 @@
 <script>
+import { fade } from 'svelte/transition'
 import { notification } from './stores.js'
 </script>
 
 {#if $notification}
-<div class="fixed inset-0 flex items-center justify-center pointer-events-none">
+<div transition:fade class="fixed inset-0 flex items-center justify-center pointer-events-none">
   <div class="px-3 py-1 bg-gray-600 text-white text-2xl rounded-lg shadow-md">
     {$notification}
   </div>
