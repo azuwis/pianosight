ea7cd09c5ef11f88cc475bc1508976729534565f
Zhong Jianxin <azuwis@gmail.com>
2020-07-25 19:37:48 +0800

    Staves: Add css style

--- src/Staves.svelte
+++ src/Staves.svelte
@@ -4,9 +4,12 @@ import { fromPairs } from "lodash-es";
 
 </script>
 
+<div class="p-2 border border-gray-400 rounded hover:border-gray-500 focus:border-gray-500">
+Staves
 {#each Array.from($allStaves) as staff}
-<label>
-  <input type=checkbox bind:group={$stavesToCheck} value={staff}>
-  {staff}
-</label>
+  <label>
+    <input type=checkbox bind:group={$stavesToCheck} value={staff}>
+    {staff}
+  </label>
 {/each}
+</div>
